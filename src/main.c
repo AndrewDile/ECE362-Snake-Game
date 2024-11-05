b5b6e4cbd4ba260d32c1c76b7aa380ee95416433
AndrewDile <dilea@purdue.edu>
2024-11-04 22:05:59 -0500

    Initial Push

---
+++ src/main.c
@@ -0,0 +1,17 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Andrew Dile, 
+  * @date    November 4 2024
+  * @brief   ECE 362 Snake Game
+  ******************************************************************************
+*/
+
+#include "stm32f0xx.h"
+#include <math.h>   // for M_PI
+#include <stdint.h>
+#include <stdio.h>
+
+int main(void) {
+    
+}
